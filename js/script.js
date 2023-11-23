6b356da37926206d7b39695ed57dd28cd2ac1e76
Quiatus <Quiatus@gmail.com>
2023-11-23 18:55:21 +0100

    add string

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 const inputBtn = document.querySelector('#input-btn');
 const inputEl = document.getElementById('input-el');
+const ulEl = document.getElementById('ulEl');
 let myLeads = [];
 
 function btnClick() {
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+
+    let listItems = "";
+
+    for (let i = 0; i < myLeads.length; i++) {
+        // listItems += "<li><a href='https://" + myLeads[i] + "' target='_blank'>"  + myLeads[i] + "</a></li>";
+        listItems += `
+        <li>
+            <a href='https://${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+        </li>`;
+    }
+
+    ulEl.innerHTML = listItems;
+    inputEl.value = "";
 }
 
 inputBtn.addEventListener('click',btnClick);
